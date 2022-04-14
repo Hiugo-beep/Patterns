d2cc770ce7ceb11cd3c833cd74211a0797136077
Mikhail <mikhail.shestak@itechart-group.com>
2022-04-14 19:30:09 +0300

    Add debuger

--- www/builder/PostBuilderImpl.php
+++ www/builder/PostBuilderImpl.php
@@ -1,45 +1,59 @@
 <?php
 
-class PostBuilderImpl implements PostBuilder{
+class PostBuilderImpl implements PostBuilder
+{
 
+    /** @var Post */
     private $post;
 
-    public function __construct(){
+    public function __construct()
+    {
         $this->create();
     }
 
-    public function create(): PostBuilder{
+    public function create(): PostBuilder
+    {
         $this->post = new Post();
         return $this;
     }
 
-    public function setTitlePost(string $val): PostBuilder{
-        $this->post->title = $val;
+    public function setTitlePost(string $val): PostBuilder
+    {
+        $this->post->setTitle($val);
         return $this;
     }
-    public function setBodyPost(string $val): PostBuilder{
+
+    public function setBodyPost(string $val): PostBuilder
+    {
         #$this->post->setBody($val);
         $this->post->body = $val;
         return $this;
     }
-    public function setAuthorPost(string $val): PostBuilder{
+
+    public function setAuthorPost(string $val): PostBuilder
+    {
         #$this->post->setAuthor($val);
         $this->post->author = $val;
         return $this;
     }
-    public function setCategoriesPost(array $val): PostBuilder{
+
+    public function setCategoriesPost(array $val): PostBuilder
+    {
         #$this->post->setCategories($val);
         $this->post->categories = $val;
         return $this;
     }
-    public function setTagsPost(array $val): PostBuilder{
+
+    public function setTagsPost(array $val): PostBuilder
+    {
         $this->post->tags = $val;
         #$this->post->setTags($val);
         return $this;
     }
 
-    public function getPost(){
-        echo (gettype($this->post));
+    public function getPost()
+    {
+        echo(gettype($this->post));
         $result = $this->post;
         $this->post = $this->create();
         return $result;
