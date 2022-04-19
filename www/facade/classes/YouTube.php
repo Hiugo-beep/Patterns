ac586e54ee2905d2900088d4c1c5bbae85dea27f
Mariya <mariekoh@yandex.ru>
2022-04-19 13:29:18 +0300

    Facade rebuilding

--- www/facade/classes/YouTube.php
+++ www/facade/classes/YouTube.php
@@ -4,16 +4,20 @@ class YouTube
 {
 
     private $videoPath;
-    public function __construct(String $url){
+
+    public function __construct(string $url)
+    {
         $this->videoPath = $url;
     }
 
-    public function fetchVideo($url): void {
+    public function fetchVideo($url): void
+    {
         echo "      ... Fetching " . $url . " ... <br>";
         echo "      ... Done! <br>";
     }
 
-    public function saveAs(string $path, string $name): void {
+    public function saveAs(string $path, string $name): void
+    {
         echo "      ... Save " . $path . " as " . $name . " ... <br>";
         echo "      ... Done! <br>";
     }
