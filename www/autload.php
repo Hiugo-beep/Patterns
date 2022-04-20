09f5be6cf80dd85b8b671ce364a52df8c49000d9
Mariya <mariekoh@yandex.ru>
2022-04-20 16:03:48 +0300

    add general autloader

---
+++ www/autload.php
@@ -0,0 +1,9 @@
+<?php
+
+include('builder/AutoloaderBuilder.php');
+include('observer/AutoloaderObserver.php');
+include('iterator/AutoloaderIterator.php');
+include('facade/AutoloaderFacade.php');
+include('factory/AutoloaderFactory.php');
+include('singleton/Singleton.php');
+include('decorator/AutoloaderDecorator.php');
